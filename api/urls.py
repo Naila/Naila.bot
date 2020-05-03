fd2212dfbd95fb27ab5790fa9ef90a9df361a8ce
Kanin <im@kanin.dev>
2020-05-03 16:47:09 -0400

    yolo

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 from .api_views import *
+from webhooks.api_views import Webhooks
 
 
 router = routers.DefaultRouter()
@@ -8,5 +9,6 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path("", include(router.urls)),
     path("ship", Ship.as_view(), name="ship"),
+    path("webhook", Webhooks.as_view(), name="webhook"),
     path("api-auth/", include("rest_framework.urls"))
 ]
---
+++ webhooks/api_views.py
@@ -0,0 +1,6 @@
+from rest_framework.views import APIView
+
+
+class Webhooks(APIView):
+    def post(self, request):
+        print(request)
