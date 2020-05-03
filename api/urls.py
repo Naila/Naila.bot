6ed10e161b00c32e44ab59dd8465e4c63db24e06
Kanin <im@kanin.dev>
2020-05-03 17:49:12 -0400

    Testing github

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 from .api_views import *
-from webhooks.api_views import Webhooks
+from webhooks.api_views import GithubWebhooks
 
 
 router = routers.DefaultRouter()
@@ -9,6 +9,6 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path("", include(router.urls)),
     path("ship", Ship.as_view(), name="ship"),
-    path("webhook", Webhooks.as_view(), name="webhook"),
+    path("webhook/github", GithubWebhooks.as_view(), name="webhook/github"),
     path("api-auth/", include("rest_framework.urls"))
 ]
--- webhooks/api_views.py
+++ webhooks/api_views.py
@@ -1,6 +1,25 @@
+import hmac
+import os
+
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-class Webhooks(APIView):
+class GithubWebhooks(APIView):
     def post(self, request):
-        print(request.headers)
+        secret = os.getenv("GITHUB-SECRET")
+        header_signature = request.headers.get("X-Hub-Signature")
+        sha_name, signature = header_signature.split("=")
+        if sha_name != "sha1":
+            return Response({"message": "sha1 required"}, status=status.HTTP_400_BAD_REQUEST)
+        mac = hmac.new(str(secret), msg=request.data, digestmod="sha1")
+        if not str(mac.hexdigest() == str(signature)):
+            return Response({"message": "invalid secret"}, status=status.HTTP_403_FORBIDDEN)
+
+        event = request.headers.get("X-GitHub-Event", "ping")
+        if event == "ping":
+            return Response("Pong!")
+
+        print(event)
+        print(request.data)
