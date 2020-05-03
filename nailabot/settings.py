f02241aa2482faa6c330e84b41564d9d9e52583d
Kanin <im@kanin.dev>
2020-05-03 10:58:24 -0400

    Fixing allowed hosts

--- nailabot/settings.py
+++ nailabot/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") != "False"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "naila.bot"]
 
 # Application definition
 
