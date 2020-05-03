f5560d9b12718f155b98b528150398d61d178361
Kanin <im@kanin.dev>
2020-05-03 18:24:07 -0400

    encode

--- webhooks/api_views.py
+++ webhooks/api_views.py
@@ -13,7 +13,7 @@ class GithubWebhooks(APIView):
         sha_name, signature = header_signature.split("=")
         if sha_name != "sha1":
             return Response({"message": "sha1 required"}, status=status.HTTP_400_BAD_REQUEST)
-        mac = hmac.new(str(secret).encode("utf8"), msg=request.data, digestmod="sha1")
+        mac = hmac.new(str.encode(secret), msg=request.data, digestmod="sha1")
         if not str(mac.hexdigest() == str(signature)):
             return Response({"message": "invalid secret"}, status=status.HTTP_403_FORBIDDEN)
 
