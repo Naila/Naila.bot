0ed3292287f188fe4efb084adb1cb9aba49dc4b5
Kanin <im@kanin.dev>
2020-05-03 23:05:31 -0400

    Testing github sponsors webhook forwarding

--- webhooks/api_views.py
+++ webhooks/api_views.py
@@ -1,7 +1,10 @@
-import hmac
-import os
 import hashlib
+import hmac
 import json
+import os
+from datetime import datetime
+
+import requests
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,7 +12,7 @@ from rest_framework.views import APIView
 
 class GithubWebhooks(APIView):
     def post(self, request):
-        secret = os.getenv("GITHUB-SECRET")
+        secret = os.getenv("GITHUB_SECRET")
         header_signature = request.headers.get("X-Hub-Signature")
         sha_name, signature = header_signature.split("=")
         if sha_name != "sha1":
@@ -23,5 +26,40 @@ class GithubWebhooks(APIView):
         if event == "ping":
             return Response("Pong!")
 
+        if event == "sponsorship":
+            action = request.data["action"]
+            public = "private" not in request.data["sponsorship"]["privacy_level"]
+            created = request.data["sponsorship"]["created_at"]
+            sponsor_data = request.data["sponsorship"]["sponsor"]
+            maintainer_data = request.data["sponsorship"]["maintainer"]
+            tier_data = request.data["sponsorship"]["tier"]
+            webhook_data = {}
+            embed = {
+                "timestamp": created,
+                "color": 65370,
+                "thumbnail": {
+                    "url": sponsor_data["html_url"]
+                },
+                "author": {
+                    "name": "New sponsor!",
+                    "url": f"https://github.com/sponsors/{maintainer_data['login']}",
+                    "icon_url": maintainer_data["avatar_url"]
+                },
+                "fields": [
+                    {
+                        "name": "Sponsor:",
+                        "value": f"[**{sponsor_data['login']}**]({sponsor_data['html_url']})"
+                    },
+                    {
+                        "name": "Tier:",
+                        "value": f"**Name:** {tier_data['name']}\n**Price:** ${tier_data['monthly_price_in_dollars']}"
+                    }
+                ]
+            }
+            webhook_data["embeds"] = [embed]
+            webhook_data["username"] = "GitHub Sponsor"
+            webhook_data["avatar_url"] = "https://kanin.naila.bot/2020/05/04/tPy1JU.png"
+            requests.post(url=os.getenv("GITHUB_SPONSORS_WEBHOOK"), json=webhook_data)
+            return Response("Forwarded!")
+
         print(event)
-        print(request.data)
