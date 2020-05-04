2e8f83479aee2d6586b1673fe8bbad64d919b19f
Kanin <im@kanin.dev>
2020-05-03 23:22:00 -0400

    Finishing sponsors

--- webhooks/api_views.py
+++ webhooks/api_views.py
@@ -2,7 +2,6 @@ import hashlib
 import hmac
 import json
 import os
-from datetime import datetime
 
 import requests
 from rest_framework import status
@@ -15,9 +14,10 @@ class GithubWebhooks(APIView):
         secret = os.getenv("GITHUB_SECRET")
         header_signature = request.headers.get("X-Hub-Signature")
         sha_name, signature = header_signature.split("=")
+
         if sha_name != "sha1":
             return Response({"message": "sha1 required"}, status=status.HTTP_400_BAD_REQUEST)
-        print(type(request.data))
+
         mac = hmac.new(str.encode(secret), msg=json.dumps(request.data).encode("utf8"), digestmod=hashlib.sha1)
         if not str(mac.hexdigest() == str(signature)):
             return Response({"message": "invalid secret"}, status=status.HTTP_403_FORBIDDEN)
@@ -33,33 +33,41 @@ class GithubWebhooks(APIView):
             sponsor_data = request.data["sponsorship"]["sponsor"]
             maintainer_data = request.data["sponsorship"]["maintainer"]
             tier_data = request.data["sponsorship"]["tier"]
-            webhook_data = {}
-            embed = {
-                "timestamp": created,
-                "color": 65370,
-                "thumbnail": {
-                    "url": sponsor_data["avatar_url"]
-                },
-                "author": {
-                    "name": "New sponsor!",
-                    "url": f"https://github.com/sponsors/{maintainer_data['login']}",
-                    "icon_url": maintainer_data["avatar_url"]
-                },
-                "fields": [
-                    {
-                        "name": "Sponsor:",
-                        "value": f"[**{sponsor_data['login']}**]({sponsor_data['html_url']})"
+            author_name = "Fix this"
+            color = 16711680
+            if action == "created":
+                author_name = "New Sponsor!"
+                color = 65370
+            webhook_data = {
+                "username": "GitHub Sponsor",
+                "avatar_url": "https://kanin.naila.bot/2020/05/04/tPy1JU.png",
+                "embeds": [{
+                    "timestamp": created,
+                    "color": color,
+                    "thumbnail": {
+                        "url": sponsor_data["avatar_url"]
                     },
-                    {
-                        "name": "Tier:",
-                        "value": f"**Name:** {tier_data['name']}\n**Price:** ${tier_data['monthly_price_in_dollars']}"
-                    }
-                ]
+                    "author": {
+                        "name": author_name,
+                        "url": f"https://github.com/sponsors/{maintainer_data['login']}",
+                        "icon_url": maintainer_data["avatar_url"]
+                    },
+                    "fields": [
+                        {
+                            "name": "Sponsor:",
+                            "value": f"[**{sponsor_data['login']}**]({sponsor_data['html_url']})"
+                        },
+                        {
+                            "name": "Tier:",
+                            "value": f"**Name:** {tier_data['name']}\n**Price:** ${tier_data['monthly_price_in_dollars']}"
+                        }
+                    ]
+                }]
             }
-            webhook_data["embeds"] = [embed]
-            webhook_data["username"] = "GitHub Sponsor"
-            webhook_data["avatar_url"] = "https://kanin.naila.bot/2020/05/04/tPy1JU.png"
-            requests.post(url=os.getenv("GITHUB_SPONSORS_WEBHOOK"), json=webhook_data)
-            return Response("Forwarded!")
+            if public:
+                requests.post(url=os.getenv("GITHUB_SPONSORS_WEBHOOK_PUBLIC"), json=webhook_data)
+            else:
+                requests.post(url=os.getenv("GITHUB_SPONSORS_WEBHOOK_PRIVATE"), json=webhook_data)
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
         print(event)
